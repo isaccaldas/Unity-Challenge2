15468ea9a0a909ac72aa01bdcf90db11996d51fd
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-08-18 23:26:46 -0300

    mod the cube

--- Mod the Cube/Assets/ModTheCube/Cube.cs
+++ Mod the Cube/Assets/ModTheCube/Cube.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+    private float startDelay = 1.0f;
+    private float spawnInterval = 2f;
+   
     void Start()
     {
-         transform.position = new Vector3(3, 4, 1);
-         transform.localScale = Vector3.one * 1.3f;
         
-        Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 1.0f, 1.4f);
+         InvokeRepeating("RandomFeatures", startDelay, spawnInterval);
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 20.0f * Time.deltaTime, 30.0f * Time.deltaTime);
+        transform.Rotate(10.0f * Time.deltaTime, 20.0f * Time.deltaTime, 30.0f * Time.deltaTime);  
+    }
+
+    void RandomFeatures(){
+
+        Material material = Renderer.material;
+         
+        material.color = new Color(Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), 
+                                    Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f));
+
+        transform.position = new Vector3(Random.Range(-5.0f,5.0f), Random.Range(-3.0f,3.0f), 
+                                        Random.Range(-1.0f,1.0f));
+        transform.localScale = Vector3.one * Random.Range(0.5f,5.0f);
+
+
     }
 }
